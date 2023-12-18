e3cd2749806857059cea9497c8915e60532f0e0f
alejandroCG <alejandroalejandro1102@gmail.com>
2023-12-17 21:40:49 -0600

    adding components

--- DemoApp/src/app/app.module.ts
+++ DemoApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
